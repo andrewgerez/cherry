211eebf5bd7220799e40336da41cece9ee4ea2bc
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-18 20:40:44 -0300

    feat: /links post route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { sql } from './lib/postgres'
 
 const app = fastify()
+
+app.post('/links', async (req, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  })
+
+  const { code, url } = createLinkSchema.parse(req.body)
+
+  const result = await sql/*sql */`
+    INSERT INTO cherry_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id
+  `
+
+  const link = result[0]
+
+  return reply.status(201).send({ cherryLinkId: link.id })
+})
 
 app.listen({
   port: 3333,
